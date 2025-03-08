12cb4b0978a759b5381d696addf69eaba366ca04
nadinnah <nadinahmedmohamed@gmail.com>
2025-03-09 00:05:30 +0200

    added the page in MainActivity

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -7,11 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.taskmanager.tasks.login.LoginViewModel
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +23,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val loginViewModel= viewModel(modelClass = LoginViewModel::class.java)
+            TaskManagerTheme {
+                Surface(modifier= Modifier.fillMaxSize(),
+                    color= MaterialTheme.colorScheme.background) {
+                    Navigation(loginViewModel=loginViewModel)
+                }
+
+            }
             Column {
                 Text("hello")
                 Text("hello")
