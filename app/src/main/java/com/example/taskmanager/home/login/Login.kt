ec18db10734ef60b84788c92ca8b702242aadfc1
nadinnah <nadinahmedmohamed@gmail.com>
2025-03-08 21:14:47 +0200

    finished login screen base

--- app/src/main/java/com/example/taskmanager/home/login/Login.kt
+++ app/src/main/java/com/example/taskmanager/home/login/Login.kt
@@ -1,22 +1,31 @@
 package com.example.taskmanager.home.login
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
@@ -37,9 +46,10 @@ fun LoginScreen(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.padding(30.dp))
         Text(
             text = "Login",
-            style = MaterialTheme.typography.headlineMedium,
+            style = MaterialTheme.typography.headlineLarge,
             fontWeight = FontWeight.Black,
             color = MaterialTheme.colorScheme.primary
         )
@@ -49,20 +59,160 @@ fun LoginScreen(
                 color = Color.Red
             )
         }
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            value = loginUiState?.userName ?: "",
+            onValueChange = { loginViewModel?.onUserNameChange(it) },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Person, contentDescription = null)
+            },
+            label = {
+                Text(text = "Email")
+            },
+            isError = isError
+        )
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            value = loginUiState?.password ?: "",
+            onValueChange = { loginViewModel?.onPasswordChange(it) },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Lock, contentDescription = null)
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            label = {
+                Text(text = "Password")
+            },
+            isError = isError
+        )
+
+        Button(onClick = { loginViewModel?.loginUser(context) }) {
+            Text(text = "Sign in")
+
+        }
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text(text = "Don't have an account?")
+            Spacer(modifier = Modifier.padding(8.dp))
+            TextButton(onClick = { onNavToSignupPage.invoke() }) {
+                Text(text = "SignUp")
+            }
+
+        }
+
+        if(loginUiState?.isLoading==true){
+            CircularProgressIndicator()
+        }
+
+        //side effect
+        LaunchedEffect(key1 = loginViewModel?.hasUser){
+            if(loginViewModel?.hasUser==true){
+                onNavToHomePage.invoke()
+            }
+        }
+
 
     }
 
-    OutlinedTextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        value = loginUiState?.userName ?: "",
-        onValueChange = { loginViewModel?.onUserNameChange(it) },
-        leadingIcon = {
-           Icon(imageVector = Icons.Default.Person, contentDescription =null)
-        },
-        label={}
-    )
+
+}
+
+@Composable
+fun SignUpScreen(
+    loginViewModel: LoginViewModel? = null,
+    onNavToHomePage: () -> Unit,
+    onNavToLoginPage: () -> Unit,
+    onNavToSignupPage: () -> Unit
+) {
+    val loginUiState = loginViewModel?.loginUiState
+    val isError = loginUiState?.loginError != null
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.padding(30.dp))
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Black,
+            color = MaterialTheme.colorScheme.primary
+        )
+        if (isError) {
+            Text(
+                text = loginUiState?.loginError ?: "unknown error",
+                color = Color.Red
+            )
+        }
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            value = loginUiState?.userName ?: "",
+            onValueChange = { loginViewModel?.onUserNameChange(it) },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Person, contentDescription = null)
+            },
+            label = {
+                Text(text = "Email")
+            },
+            isError = isError
+        )
+
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            value = loginUiState?.password ?: "",
+            onValueChange = { loginViewModel?.onPasswordChange(it) },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Lock, contentDescription = null)
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            label = {
+                Text(text = "Password")
+            },
+            isError = isError
+        )
+
+        Button(onClick = { loginViewModel?.loginUser(context) }) {
+            Text(text = "Sign in")
+
+        }
+
+        Spacer(modifier = Modifier.padding(16.dp))
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text(text = "Don't have an account?")
+            Spacer(modifier = Modifier.padding(8.dp))
+            TextButton(onClick = { onNavToSignupPage.invoke() }) {
+                Text(text = "SignUp")
+            }
+
+        }
+
+        if(loginUiState?.isLoading==true){
+            CircularProgressIndicator()
+        }
+
+        //side effect
+        LaunchedEffect(key1 = loginViewModel?.hasUser){
+            if(loginViewModel?.hasUser==true){
+                onNavToHomePage.invoke()
+            }
+        }
+
+
+    }
+
+
 }
 
 @Preview(showSystemUi = true)
