082e344f8658b8ba7d55c7f9d40e4f5921392aaf
nadinnah <nadinahmedmohamed@gmail.com>
2025-03-13 13:50:40 +0200

    add StorageRepository for firebase

--- app/src/main/java/com/example/taskmanager/tasks/domain/repository/StorageRepository.kt
+++ app/src/main/java/com/example/taskmanager/tasks/domain/repository/StorageRepository.kt
@@ -1,2 +1,27 @@
 package com.example.taskmanager.tasks.domain.repository
 
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+const val TASKS_COLLECTION_REF= "tasks"
+
+class StorageRepository(){
+
+    val user= Firebase.auth.currentUser
+
+    fun hasUser(): Boolean= Firebase.auth.currentUser!=null
+
+    fun getUserId(): String= Firebase.auth.currentUser?.uid.orEmpty()
+
+    private val tasksRef= Firebase.firestore.collection(TASKS_COLLECTION_REF)
+
+
+}
+
+//manages the state of getting the data
+sealed class Resources<T>(val data:T?=null, val throwable:Throwable?= null){
+    class Loading<T>:Resources<T>()
+    class Success<T>(data: T): Resources<T>(data= data)
+    class Error<T>(throwable: Throwable?):Resources<T>(throwable = throwable)
+}
