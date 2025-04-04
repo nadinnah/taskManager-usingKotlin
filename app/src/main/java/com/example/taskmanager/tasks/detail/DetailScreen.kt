498b8caa56e6566f535b3e51d81ace7989a7bd92
nadinnah <nadinahmedmohamed@gmail.com>
2025-04-04 20:57:28 +0200

    added snackbars in detailScreen for added tasks and updated tasks

--- app/src/main/java/com/example/taskmanager/tasks/detail/DetailScreen.kt
+++ app/src/main/java/com/example/taskmanager/tasks/detail/DetailScreen.kt
@@ -1,5 +1,12 @@
-
+import android.annotation.SuppressLint
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Refresh
@@ -9,35 +16,77 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import com.example.taskmanager.tasks.Utils
 import com.example.taskmanager.tasks.detail.DetailUIState
 import com.example.taskmanager.tasks.detail.DetailViewModel
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "UnusedMaterialScaffoldPaddingParameter",
+    "CoroutineCreationDuringComposition"
+)
 @Composable
 fun DetailScreen(
     detailViewModel: DetailViewModel?,
     taskId: String,
     onNavigate: () -> Unit
 ) {
-    val detailUiState= detailViewModel?.detailUiState?: DetailUIState()
+    val detailUiState = detailViewModel?.detailUiState ?: DetailUIState()
 
-    val isFormsNotBlank= detailUiState.task.isNotBlank() && detailUiState.taskTitle.isNotBlank()
+    val isFormsNotBlank = detailUiState.task.isNotBlank() && detailUiState.taskTitle.isNotBlank()
 
     val selectedColor by animateColorAsState(targetValue = Utils.colors[detailUiState.colorIndex])
 
-    val isTaskIdNotBlank= taskId.isNotBlank()
+    val isTaskIdNotBlank = taskId.isNotBlank()
 
-    val icon= if(isFormsNotBlank) Icons.Default.Refresh else Icons.Default.Check
+    val icon = if (isFormsNotBlank) Icons.Default.Refresh else Icons.Default.Check
 
-    LaunchedEffect(key1 = Unit){
-        if(isTaskIdNotBlank){
+    LaunchedEffect(key1 = Unit) {
+        if (isTaskIdNotBlank) {
             detailViewModel?.getTask(taskId)
-        }else{
+        } else {
             detailViewModel?.resetState()
         }
     }
 
     val scope = rememberCoroutineScope()
-    val scaffoldState= rememberScaffoldState()
+    val scaffoldState = rememberScaffoldState()
 
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                if (isTaskIdNotBlank) {
+                    detailViewModel?.updateTask(taskId)
+                } else {
+                    detailViewModel?.addTask()
+                }
+            }) {
+                Icon(imageVector = icon, contentDescription = null)
+            }
+        },
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = selectedColor)
+                .padding(padding)
+        ) {
+            if(detailUiState.taskAddedStatus){
+                scope.launch{
+                    scaffoldState.snackbarHostState.showSnackbar("Task has been added successfully")
+                    detailViewModel?.resetNoteAddedStatus()
+                    onNavigate.invoke()
+
+                }
+            }
+            if(detailUiState.updateTaskStatus) {
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar("Task has been updated successfully")
+                    detailViewModel?.resetNoteAddedStatus()
+                    onNavigate.invoke()
+                }
+            }
+        }
+    }
 }
